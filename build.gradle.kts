309a7718580d71330681dd0a0c626d0f7c240f67
wreulicke <wreulicke@gmail.com>
2024-06-01 01:55:51 +0900

    add setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@
  */
 import net.ltgt.gradle.errorprone.CheckSeverity
 import net.ltgt.gradle.errorprone.errorprone
+import cl.franciscosolis.sonatypecentralupload.SonatypeCentralUploadTask
 
 plugins {
     `java-library`
@@ -13,9 +14,11 @@ plugins {
     `jacoco`
     alias(libs.plugins.errorprone)
     alias(libs.plugins.spotless)
+    id("cl.franciscosolis.sonatype-central-upload") version "1.0.2"
 }
 
-group = "com.github.wreulicke.errorprone.logstash"
+group = "io.github.wreulicke.errorprone.logstash"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -40,6 +43,8 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+    withSourcesJar()
+    withJavadocJar()
 }
 
 
@@ -107,6 +112,67 @@ spotless {
         target("**/*.gradle.kts")
         indentWithSpaces()
     }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            pom {
+                name.set(project.name)
+                description.set("Custom Errorprone check for logstash-logback-encoder")
+                url.set("https://github.com/wreulicke/errorprone-logstash-logback-encoder")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/wreulicke/errorprone-logstash-logback-encoder/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("wreulicke")
+                        name.set("wreulicke")
+                        email.set("wreulicke@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:wreulicke/errorprone-logstash-logback-encoder.git")
+                    developerConnection.set("scm:git:git@github.com:wreulicke/errorprone-logstash-logback-encoder.git")
+                    url.set("https://github.com/wreulicke/errorprone-logstash-logback-encoder")
+                }
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("https://github.com/wreulicke/errorprone-logstash-logback-encoder/issues")
+                }
+            }
+        }
+    }
+}
+
+tasks.named<SonatypeCentralUploadTask>("sonatypeCentralUpload") {
+    // 公開するファイルを生成するタスクに依存する。
+    dependsOn("jar", "sourcesJar", "javadocJar", "generatePomFileForMavenPublication")
+
+    // Central Portalで生成したトークンを指定する。
+    username = System.getenv("SONATYPE_CENTRAL_USERNAME")
+    password = System.getenv("SONATYPE_CENTRAL_PASSWORD")
+
+    // タスク名から成果物を取得する。
+    archives = files(
+        tasks.named("jar"),
+        tasks.named("sourcesJar"),
+        tasks.named("javadocJar"),
+    )
+    // POMファイルをタスクの成果物から取得する。
+    pom = file(
+        tasks.named("generatePomFileForMavenPublication").get().outputs.files.single()
+    )
+
+    // PGPの秘密鍵を指定する。
+    signingKey = System.getenv("PGP_SIGNING_KEY")
+    // PGPの秘密鍵のパスフレーズを指定する。
+    signingKeyPassphrase = System.getenv("PGP_SIGNING_KEY_PASSPHRASE")
 }
 
 defaultTasks("spotlessApply", "build")
